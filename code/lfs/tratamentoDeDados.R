11a98734df2f0b82e6a8795aa7b52bbc90edfa9c
TsunekiR <tsunekiroberto@gmail.com>
2019-11-12 16:16:07 -0200

    atualizado o código para incluir ano e geometria, porém necessita de bastante memória RAM

--- code/lfs/tratamentoDeDados.R
+++ code/lfs/tratamentoDeDados.R
@@ -1,7 +1,7 @@
 startTime <- Sys.time()
-library(maptools)
 library(dplyr)
 library(foreign)
+library(sf)
 
 #lendo a tabela LOG2018_CEM_RMSP.dbf
 tabela <- read.dbf("/home/tsuneki/Downloads/FAU/LOG2018/LOG2018_CEM_RMSP.dbf", as.is = F)[,c(6,19)]
@@ -12,46 +12,49 @@ df <- bind_rows(tabela)
 
 #retirando os valores nulos do CEP
 tabela <- df[!is.na(df$CEP_E),]
-View(tabela)
+
+rm(df)
 
 #lendo a tabela do IPTU de 1995
-IPTU1995 <- read.csv2("/home/tsuneki/Downloads/FAU/csv/IPTU_1997.csv", header = T, sep = ";", stringsAsFactors = F, dec = ",", quote = "")[,c(7,18,20,21,22,23,24,25,32)]
-
-View(IPTU1995)
+tab <- read.csv2("/home/tsuneki/Downloads/FAU/csv/IPTU_1995.csv", header = T, sep = ";", stringsAsFactors = F, dec = ",", quote = "")[,c(7,18,20,21,22,23,24,25,32)]
 
 #reformatando os CEPs
 for(i in 1:length(tabela[,2])){
   tabela[i, 3] <- paste(substring(tabela[i, 2], c(1,6), c(5,8)), collapse = "-")
 }
 
-#retiando a coluna com os CEPs nao formatados
+#retirando a coluna com os CEPs nao formatados
 tabela <- tabela[c(1,3)]
 names(tabela) <- c("Bairro","CEP.DO.IMOVEL")
 
-View(tabela)
 
 #criando um dataframe com a juncao das tabelas a partir do CEP
-tabelaFim95 <- data_frame()
-tabelaFim95 <- left_join(IPTU1995, tabela, by = "CEP.DO.IMOVEL")  
-tabelaFim95 <- tabelaFim95 %>% distinct()
-tabelaFim95 <- tabelaFim95[!is.na(tabelaFim95$Bairro),]
+aux <- data_frame()
+aux <- left_join(tab, tabela, by = "CEP.DO.IMOVEL")  
+aux <- aux %>% distinct()
+aux <- aux[!is.na(aux$Bairro),]
 
-View(tabelaFim95)
+rm(tab, tabela)
 
 #tratando a tabela dos bairros desejados
 bairros <- read.csv("/home/tsuneki/Downloads/FAU/csv/Distrito_zonas.csv", sep = ",", header = T)
 names(bairros) <- c("CaD","Bairro","Z. Fiscal")
 bairros <-bairros[c(2,3)]
 
-View(bairros)
+#tratando a tebaela de geolocalizacoes
+geo <- st_read("/home/tsuneki/Downloads/FAU/LOG2018/LOG2018_CEM_RMSP.shp")[c(6,26)]
+geo <- geo[!is.na(geo$BAIRRO),]
 
-#criando a tabela com os dados e bairros desejados
-tabelaFimBairros <- inner_join(tabelaFim95, bairros, by = "Bairro")  
+colnames(geo)[1] <- "Bairro"
 
-View(tabelaFimBairros)
+geo <- geo %>% mutate(Bairro = as.character(Bairro))
 
-#criacao de um .csv da tabela tratada
-write.csv(tabelaFimBairros, "/home/tsuneki/Downloads/FAU/csv/tabelaFinal.csv")
+#criando a tabela com os dados, bairros e geolocalkizacoes desejados
+tabelaFimBairros <- inner_join(aux, bairros, by = "Bairro")  
+tabelaFimBairros <- inner_join(tabelaFimBairros, geo, by = "Bairro") 
+tabelaFimBairros <- tabelaFimBairros %>% distinct()
+
+rm(aux,bairros,geo)
 
 #calculo do valor venal
 for(i in 1:length(tabelaFimBairros[,1])){
@@ -69,11 +72,25 @@ for(i in 1:length(tabelaFimBairros[,1])){
   
   tabelaFimBairros[i,12] <- paste(VVI)
 }
+#adicionar ano na tabela
+tabelaFimBairros[,13] <- 95
+
 #renomeacao das colunas
-names(tabelaFimBairros) = c("NOME.DO.CONTRIBUINTE", "CEP.DO.IMOVEL","FRACAO.IDEAL","AREA.DO.TERRENO","AREA.CONSTRUIDA","AREA.OCUPADA","VALOR.DO.M2.DO.TERRENO","VALOR.DO.M2.DE.CONSTRUCAO","FATOR.DE.OBSOLESCENCIA","BAIRRO","Z.FISCAL","VVI")
+names(tabelaFimBairros) = c("NOME.DO.CONTRIBUINTE", "CEP.DO.IMOVEL","FRACAO.IDEAL","AREA.DO.TERRENO","AREA.CONSTRUIDA","AREA.OCUPADA","VALOR.DO.M2.DO.TERRENO","VALOR.DO.M2.DE.CONSTRUCAO","FATOR.DE.OBSOLESCENCIA","BAIRRO","Z.FISCAL","VVI","ANO")
 
-View(tabelaFimBairros)
+#criação da tabela final
+aux2 <- inner_join(tabelaFimBairros, geo, by = "BAIRRO") 
 
-write.csv(tabelaFimBairros, "/home/tsuneki/Downloads/FAU/csv/tabelaFinal97.csv")
+rm(tabelaFimBairros)
+
+View(aux2)
+
+#criacao de um .csv da tabela tratada
+#write.csv(tabelaFimBairros, "/home/tsuneki/Downloads/FAU/git/iptu/planilhas/resultados/tabelaFinal19.csv")
 endTime <- Sys.time()
 endTime - startTime
+
+
+
+
+
